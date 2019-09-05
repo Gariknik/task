42d59017da8d1931f06a76a17927365ef1ba8e35
Igor Nikitenko <nikitenkoivi@gmail.com>
2019-09-05 12:50:39 +0300

    four task

--- skript.js
+++ skript.js
@@ -85,8 +85,6 @@ for (let key in appData) {
     console.log ("Наша программа включает в себя данные: " + key + " - " + appData[key]);
 }
 
-// используем цикл While
-
 // let i = 0;
 // while (i < 2) {
 //     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
